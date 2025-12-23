ea751915a275f0075985d4ffd66544267f7e7879
gebornancin <gebornancin@gmail.com>
2025-12-22 22:34:24 -0300

    feat: add login component and custom input component
    - Updated package.json to include autoprefixer and updated postcss and tailwindcss versions.
    - Removed placeholder content from app.html and added a router outlet.
    - Defined a new route for the login component in app.routes.ts.
    - Created a new login component with its HTML, CSS, and spec files.
    - Implemented a custom input component with its HTML, CSS, and spec files.

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CustomInput } from '../../shared/components/custom-input/custom-input';
+
+@Component({
+  selector: 'app-login',
+  imports: [CustomInput],
+  templateUrl: './login.html',
+  styles: ``,
+  standalone: true,
+})
+export class Login {
+
+}
---
+++ src/app/shared/components/custom-input/custom-input.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-input',
+  imports: [CommonModule, ReactiveFormsModule], 
+  templateUrl: './custom-input.html',
+  styleUrl: './custom-input.css',
+  standalone: true,
+})
+export class CustomInput {
+  @Input() label: string = '';
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+
+  @Input() control!: FormControl;
+
+  
+}
